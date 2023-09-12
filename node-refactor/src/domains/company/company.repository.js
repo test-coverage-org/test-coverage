844de1547462aca3e65ff95e45e5f11790802391
maxiadaro <maximiliano.adaro@capchase.com>
2023-09-12 18:13:28 -0300

    no test added

--- node-refactor/src/domains/company/company.repository.js
+++ node-refactor/src/domains/company/company.repository.js
@@ -4,4 +4,8 @@ module.exports = {
   async getAllCompanies() {
     return await models.Company.findAll({});
   },
+
+  async findCompanyById(companyId) {
+    return await models.Company.findByPk(companyId);
+  },
 };
