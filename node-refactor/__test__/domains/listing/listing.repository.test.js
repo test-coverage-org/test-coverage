107092ba22affc3ac7a22918b4fb3bedacf8d524
maxiadaro <maximiliano.adaro@capchase.com>
2023-09-15 12:12:54 -0300

    country tests

--- node-refactor/__test__/domains/listing/listing.repository.test.js
+++ node-refactor/__test__/domains/listing/listing.repository.test.js
@@ -84,8 +84,14 @@ describe('getListingData', () => {
 });
 
 describe('getAllCountries', () => {
+  beforeAll(async () => {
+    // Set up the database and create the necessary models
+    await createListingsData(); //todo: create a function to create a listing with all the necessary data, not everything is needed
+  });
+
   test('get all countries', async () => {
     const result = await getAllCountries();
     expect(result).not.toBeNull();
+    expect(result).toHaveLength(3);
   });
 });
