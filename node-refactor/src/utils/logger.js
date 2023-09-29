ed0603a15784c01151ca53a093f58f8a09811cae
maxiadaro <maximiliano.adaro@capchase.com>
2023-09-29 01:00:36 -0300

    sonar

--- node-refactor/src/utils/logger.js
+++ node-refactor/src/utils/logger.js
@@ -1,10 +1,11 @@
 // Logger configuration using pino
 const pino = require('pino');
-const { NodeEnv } = require('./constants');
+// const { NodeEnv } = require('./constants');
 
 // Create a logging instance
 const logger = pino({
-  level: process.env.NODE_ENV === NodeEnv.PROD ? 'info' : 'debug',
+  // level: process.env.NODE_ENV === NodeEnv.PROD ? 'info' : 'debug',
+  level: 'info',
   transport: {
     target: 'pino-pretty',
     options: {
