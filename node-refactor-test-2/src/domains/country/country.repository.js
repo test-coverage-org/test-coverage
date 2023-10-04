bbdf5f0f2cc2e299a21e855c6138caa29e3d0c67
maxiadaro <maximiliano.adaro@capchase.com>
2023-10-04 15:51:33 -0300

    sonarcloud on push

--- node-refactor-test-2/src/domains/country/country.repository.js
+++ node-refactor-test-2/src/domains/country/country.repository.js
@@ -2,18 +2,18 @@ const models = require('@src/db/models');
 
 module.exports = {
   async getAllCountries() {
-    return await models.Country.findAll({});
+    return await models.Country.findAll({}); //
   },
 
   async getById(countryId) {
-    return await models.Country.findByPk(countryId);
+    return await models.Country.findByPk(countryId); //
   },
 
   async createCountry(country) {
-    return await models.Country.create(country);
+    return await models.Country.create(country); //
   },
 
   async addAnotherCountry(country) {
-    return await models.Country.create(country);
+    return await models.Country.create(country); //
   }
 };
